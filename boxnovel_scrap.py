b770adcc61d0cfb1d37e53879eb70e5f075e8f7f
dibyanshu <dibyanshupandey97@gmail.com>
2023-11-07 18:08:57 +0530

    adding new sample epub file for reference and updating the code

--- boxnovel_scrap.py
+++ boxnovel_scrap.py
@@ -3,5 +3,7 @@ import requests
 
 r = requests.get('https://boxnovel.com/novel/abe-the-wizard-boxnovel/chapter-439/')
 soup = BeautifulSoup(markup=r.content,features="lxml")
-chp_content = soup.find(id="content")
+# chp_content = soup.find(id="content") -: is same as :- soup.find("div",{"id":"content"})
+chp_content = soup.find("div",{"id":"content"})
+#chp = chp_content.find("p",recursive:=True)
 print(chp_content)
